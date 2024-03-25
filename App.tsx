3916710ee0f96e5ed504f6a66ac21f7444f36236
Carla Teixeira <carlateixxeira@gmail.com>
2024-03-24 22:28:21 -0300

    Commit aula 18/03

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TextInput, View } from "react-native";
-
+import React from "react";
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 export default function App() {
+  const[novaVariavel1, setNovaVariavel1] = React.useState('');
+  const[novaVariavel2, setNovaVariavel2] = React.useState('');
+  const calculo = (parseFloat(novaVariavel1) + parseFloat(novaVariavel2)) / 2;
   return (
     <View style={styles.container}>
-      <Text style={styles.titulo}>Digite seu nome:</Text>
-      <TextInput style={styles.textinp} />
-      <StatusBar style="auto" />
+      <Text style={styles.field}>Digite número 1:</Text>
+      <TextInput onChangeText={setNovaVariavel1} keyboardType="numeric" style={styles.textField}/>
+
+      <Text style={styles.field}>Digite número 2:</Text>
+      <TextInput onChangeText={setNovaVariavel2} keyboardType="numeric" style={styles.textField}/>
+
+      <TouchableOpacity style={styles.button}>
+      <Text style={styles.field}>Enviar</Text>
+      </TouchableOpacity>
+      <Text> </Text>
+      {!isNaN(calculo) && <Text style={styles.field}>Resultado: {calculo}</Text>}
+
+      <StatusBar style="auto"/>
     </View>
   );
 }
@@ -14,16 +27,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#f2f2f2",
     alignItems: "center",
     justifyContent: "center",
     paddingHorizontal: 10,
   },
-  titulo: {
-    fontSize: 30,
+
+  field: {
+    fontSize: 25,
     color: "black",
   },
-  textinp: {
+
+  textField: {
     width: "100%",
     height: 50,
     backgroundColor: "white",
@@ -33,4 +48,15 @@ const styles = StyleSheet.create({
     margin: 10,
     padding: 15,
   },
+
+  button: {
+    backgroundColor: "#fff",
+    width: "auto",
+    borderRadius: 100,
+    padding: 10,
+    marginTop: 10,
+    alignItems: 'center',
+    }
+
 });
+
